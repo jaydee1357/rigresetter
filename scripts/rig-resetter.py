892314e0e5f58259b23ca99fd07f6e4c5ab0a00a
jaydee1357 <jiten.dutia@gmail.com>
2017-11-07 15:28:28 -0800

    Update rig-resetter.py

--- scripts/rig-resetter.py
+++ scripts/rig-resetter.py
@@ -1,47 +1,31 @@
-import requests
 import json
+import requests
+import socket
 import time
 import datetime
-import socket
-
-username = "jiten.dutia@gmail.com"
-password = "@Asdf1234"
-UUID = "592c6873-99db-4dc4-b57c-9941022693ca"
-rigName = "Nvidia-Rig"
-checkDelay = 5
-rigDownCount = 0
-rigLowHashrateCount = 0
-count = 0
-url = 'https://wap.tplinkcloud.com'
-host='jaydee.duckdns.org'
-port=3335
 
 def getToken(username, password, UUID):
     payload = {"method": "login",
-               "params": {"appType": "Kasa_Android", "cloudUserName": username, "cloudPassword": password,
-                          "terminalUUID": UUID}}
+               "params": {"appType": "Kasa_Android",
+                          "cloudUserName": username,
+                          "cloudPassword": password,
+                          "terminalUUID" : UUID}}
     r = requests.post(url, data=json.dumps(payload))
     token = r.json()["result"]["token"]
     return (token)
 
+def getDeviceID(token, rigname):
+   getdevicesData = {"method": "getDeviceList"}
+   r = requests.post(url + '?token=' + myToken, data=json.dumps(getdevicesData))
+   res = r.json()["result"]["deviceList"]
+   for device in res:
+      if device["alias"]==rigName:
+         return(device["deviceId"])
 
-def checkRig(token, request):
+def getRigPowerUsage(token, request):
     r = requests.post(url + '?token=' + token, data=json.dumps(request))
     currentPower = json.loads(r.json()["result"]["responseData"])["emeter"]["get_realtime"]["power"]
-    print(str(datetime.datetime.now()) + " --> " + rigName + ": Rig Power Intake =\033[32m " + str(currentPower) + "\033[0m W")
-    if int(currentPower) <= 600:
-        result = "DOWN"
-    else:
-        result = "ACTIVE"
-    return (result)
-
-
-def getDeviceID(token, rigname):
-    getdevicesData = {"method": "getDeviceList"}
-    r = requests.post(url + '?token=' + token, data=json.dumps(getdevicesData))
-    deviceId = r.json()["result"]["deviceList"][1]["deviceId"]
-    return (deviceId)
-
+    return (currentPower)
 
 def genRequest(deviceID, request):
     commands = {'info': '{"system":{"get_sysinfo":{}}}',
@@ -60,63 +44,67 @@ def genRequest(deviceID, request):
     req = commands[request]
     return ({"method": "passthrough", "params": {"deviceId": deviceID, "requestData": req}})
 
-
-def getRigHashrate():
+def getRigHashrate(host,port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    s.send('{"id":0,"jsonrpc":"2.0","method":"miner_getstat1"}'.encode("utf-8"))
-    j=s.recv(2048)
-    s.close()
-    resp=json.loads(j.decode("utf-8"))
-    resp=resp['result']
-    hashes = int(resp[2].split(';')[0])
-    return(hashes)
+    s.settimeout(30)
+    try:
+       s.connect((host, port))
+       s.send('{"id":0,"jsonrpc":"2.0","method":"miner_getstat1"}'.encode("utf-8"))
+       j=s.recv(2048)
+       s.close()
+       resp=json.loads(j.decode("utf-8"))
+       resp=resp['result']
+       hashes = int(resp[2].split(';')[0])
+       return(hashes)
+    except socket.error:
+       return(0)
 
-
+conf_file = open('config.json')
+conf_json = json.load(conf_file)
+UUID = conf_json["UUID"]
+url = conf_json["json_url"]
+username = conf_json["tp_link_username"]
+password = conf_json["tp_link_password"]
+rigs = conf_json["rigs"]
+checkDelay = conf_json["check_delay"]
 print(str(datetime.datetime.now()) + " --> " + "Requesting User Token")
 myToken = getToken(username, password, UUID)
-print(str(datetime.datetime.now()) + " --> " + "Requesting Device ID")
-myDeviceID = getDeviceID(myToken, rigName)
-print(str(datetime.datetime.now()) + " --> " + "Checking RIG Status")
-myRigStatus = checkRig(myToken, genRequest(myDeviceID, "emeter"))
-print(str(datetime.datetime.now()) + " --> " + "Requesting Rig Hashrate")
-myRigHashrate = getRigHashrate()
-print(str(datetime.datetime.now()) + " --> RIG current hashrate is: \033[32m" + str(myRigHashrate/1000) + "\033[0m Mh/s")
-print(str(datetime.datetime.now()) + " --> RIG is \033[32m" + myRigStatus + "\033[0m")
-if (myRigHashrate/1000 < 145):
-    print(str(datetime.datetime.now()) + " --> Rig Hash rate is below 145 threshold")
-    while count <= 4:
-        time.sleep(30)
-        print(str(datetime.datetime.now()) + " --> Requesting current hashrate")
-        myRigHashrate = getRigHashrate()
-        print(str(datetime.datetime.now()) + " --> Current hashrate is : \033[32m" + str(myRigHashrate/1000) + "\033[0m Mh/s")
-        rigLowHashrateCount = rigLowHashrateCount + 1
-        count = count + 1
-    if rigLowHashrateCount == 5:
-        print(str(datetime.datetime.now()) + " --> " + "Turning TP-Link Adapter OFF")
-        req = genRequest(myDeviceID, "off")
-        res = requests.post(url + '?token=' + myToken, data=json.dumps(req))
-        print(str(datetime.datetime.now()) + " --> " + "Delay 30s before sending ON Signal")
-        time.sleep(30)
-        print(str(datetime.datetime.now()) + " --> " + "Turning TP-Link Adapter ON")
-        req = genRequest(myDeviceID, "on")
-        res = requests.post(url + '?token=' + myToken, data=json.dumps(req))
-count=0
-if myRigStatus is "DOWN":
-    print(str(datetime.datetime.now()) + " --> " + "Rig is \033[32m DOWN \033[0m . Check Rig status again in " + str(checkDelay) + "secs")
-    while count <= 4:
-        myRigStatus = checkRig(myToken, genRequest(myDeviceID, "emeter"))
-        rigDownCount = rigDownCount + 1
-        count = count + 1
-        print("check " + str(count) + " : " + myRigStatus)
-        time.sleep(checkDelay)
-    print(str(datetime.datetime.now()) + " --> " + "Rig Online Status fail:" + str(rigDownCount))
-    if rigDownCount == 5:
-        print(str(datetime.datetime.now()) + " --> " + "Turning TP-Link Adapter OFF")
-        req = genRequest(myDeviceID, "off")
-        res = requests.post(url + '?token=' + myToken, data=json.dumps(req))
-        print(str(datetime.datetime.now()) + " --> " + "Delay 30s before sending ON Signal")
-        time.sleep(30)
-        print(str(datetime.datetime.now()) + " --> " + "Turning TP-Link Adapter ON")
-        req = genRequest(myDeviceID, "on")
-        res = requests.post(url + '?token=' + myToken, data=json.dumps(req))
+count = 0
+rigCheckFailCount = 0
+
+print("********************************************************************")
+
+for rig in rigs:
+   rigName = rig["rig_name"]
+   rigHost = rig["rig_ip"]
+   rigPort = rig["rig_port"]
+   rigMinHashrate = rig["rig_min_hashrate"]
+   rigMinPower = rig["rig_min_power_usage"]
+   rigMaxPower = rig["rig_max_power_usage"]
+   print(str(datetime.datetime.now()) + " --> " + "Requesting Device ID: " + rigName)
+   myDeviceID = getDeviceID(myToken, rigName)
+   print(str(datetime.datetime.now()) + " --> " + "Checking Rig Status: " + rigName)
+   myRigPower = getRigPowerUsage(myToken, genRequest(myDeviceID, "emeter"))
+   myRigHashrate = getRigHashrate(rigHost,rigPort)/1000
+   print(str(datetime.datetime.now()) + " --> CHECK " + str(count+1) + ": " + rigName + " Power: " + str(myRigPower) + " W")
+   print(str(datetime.datetime.now()) + " --> CHECK " + str(count+1) + ": " + rigName + " Hashrate: " + str(myRigHashrate) + " Mh/s")
+   if (myRigHashrate <= rigMinHashrate) | (myRigPower <= rigMinPower) | (myRigPower >= rigMaxPower):
+      while (count <= 4):
+         time.sleep(checkDelay)
+         myRigHashrate = getRigHashrate(rigHost,rigPort)
+         myRigPower = getRigPowerUsage(myToken, genRequest(myDeviceID, "emeter"))
+         print(str(datetime.datetime.now()) + " --> CHECK " + str(count+1) + ": " + rigName + " Power: " + str(myRigPower) + " W")
+         print(str(datetime.datetime.now()) + " --> CHECK " + str(count+1) + ": " + rigName + " Hashrate: " + str(myRigHashrate) + " Mh/s")
+         rigCheckFailCount = rigCheckFailCount + 1
+         count = count + 1
+      if rigLowHashrateCount == 5:
+         print(str(datetime.datetime.now()) + " --> " + "Turning TP-Link Adapter OFF")
+         req = genRequest(myDeviceID, "off")
+         print(str(datetime.datetime.now()) + " --> " + "Delay 30s before sending ON Signal")
+         res = requests.post(url + '?token=' + myToken, data=json.dumps(req))
+         time.sleep(30)
+         req = genRequest(myDeviceID, "on")
+         print(str(datetime.datetime.now()) + " --> " + "Turning TP-Link Adapter ON")
+         res = requests.post(url + '?token=' + myToken, data=json.dumps(req))
+
+print("********************************************************************")
