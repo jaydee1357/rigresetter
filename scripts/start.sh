dc551755772187688ddc6a937b88ff64bb24bfaa
jaydee1357 <jiten.dutia@gmail.com>
2017-11-07 16:10:24 -0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,7 +3,7 @@
 mkdir /etc/rigresetter
 
 wget https://raw.githubusercontent.com/jaydee1357/rigresetter/master/scripts/rig-resetter.py -O /etc/rigresetter/rig-resetter.py
-wget https://raw.githubusercontent.com/jaydee1357/rigresetter/master/scripts/conf.json -O /etc/rigresetter/conf.json
+wget https://raw.githubusercontent.com/jaydee1357/rigresetter/master/scripts/config.json -O /etc/rigresetter/config.json
 
 # start cron
 /usr/sbin/crond -f -l 8 && tail -f /var/log/cron/cron.log
